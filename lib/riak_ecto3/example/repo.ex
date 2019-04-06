bce8a945fe0ab9cde56e7604cf6ac55ca5b2a7eb
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-07 00:16:23 +0200

    Remove unused file

--- lib/riak_ecto3/example/repo.ex
+++
@@ -1,3 +0,0 @@
-defmodule RiakEcto3.Example.Repo do
-  # use Ecto.Repo, otp_app: :riak_ecto3, adapter: RiakEcto3
-end
--- test/example/repo.ex
+++ test/example/repo.ex
@@ -1,8 +1,10 @@
 defmodule RiakEcto3Test.Example.Repo do
+  @moduledoc false
   use Ecto.Repo, otp_app: :riak_ecto3, adapter: RiakEcto3, database: "riak_ecto3_test_repo"
 end
 
 defmodule RiakEcto3Test.Example.User do
+  @moduledoc false
   use RiakEcto3.Schema
   schema "users" do
     field :name, :string
