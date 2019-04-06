df11e268828a0c330492e21ff1c9c99dd90ed47c
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-06 23:49:13 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RiakEcto3.MixProject do
   def project do
     [
       app: :riak_ecto3,
-      version: "0.1.0",
+      version: "0.5.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
